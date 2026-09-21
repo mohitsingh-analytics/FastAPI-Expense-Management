e28486f35d78a612e1810fc2cc993b75ac5484ee
mohit singh <mohitsingh.bvp@gmail.com>
2026-09-22 01:19:42 +0530

    feat: logic to vaidate incoming json data using pydantic basemodel

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 import pandas as pd
 from pathlib import Path
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class ExpenseCreate(BaseModel):
+    date:str
+    category: str
+    amount: float
+    payment_method: str
+    description: str
+
 
 BASE_DIR = Path(__file__).resolve().parent
 CSV_FILE = BASE_DIR / "sample_expenses.csv"
@@ -10,6 +19,17 @@ app= FastAPI(
     title ="Expense API",
     version="1.0.0",
 )
+
+@app.post("/expenses")
+def create_expense(expense:ExpenseCreate):
+    df = pd.read_csv(CSV_FILE)
+    existing_ids = df["id"].tolist()
+    next_id = max(existing_ids) + 1
+    print(next_id)
+    return{
+        "message":"Expense Received",
+        "expense": expense
+    }
 
 @app.get("/")
 def root():
